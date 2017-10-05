3e8edad1c159977aa190f0e9400a270c65f88a6e
Sander Marechal <sander@prezent.nl>
2017-10-05 15:28:57 +0200

    Support Foundation 6

--- src/Resources/public/js/crud.js
+++ src/Resources/public/js/crud.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
 
-    $modal = $('#crud-delete-modal');
+    var $modal = $('#crud-delete-modal');
+    var version = parseInt(window.Foundation.version[0]);
 
     // Open delete modal
     $('table.crud-grid .prezent-grid-actions a.delete').on('click', function (event) {
         event.preventDefault();
         $modal.find('.button-group .delete').attr('href', $(this).attr('href'));
-        $modal.foundation('reveal', 'open');
+
+        if (version == 5) {
+            $modal.foundation('reveal', 'open');
+        } else if (version == 6) {
+            $modal.foundation('open');
+        }
     });
 
     // Close delete modal
     $modal.find('.button-group .cancel').on('click', function (event) {
         event.preventDefault();
-        $modal.foundation('reveal', 'close');
+
+        if (version == 5) {
+            $modal.foundation('reveal', 'close');
+        } else if (version == 6) {
+            $modal.foundation('close');
+        }
     });
 
 });
