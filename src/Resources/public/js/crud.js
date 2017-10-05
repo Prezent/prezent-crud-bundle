ebf1aa312513442e44d797b041937f9ba5a35097
Sander Marechal <sander@prezent.nl>
2017-10-05 17:00:36 +0200

    Drop Foundation 5 support (use 0.3.x branch for that)

--- src/Resources/public/js/crud.js
+++ src/Resources/public/js/crud.js
@@ -1,29 +1,18 @@
 $(document).ready(function() {
 
     var $modal = $('#crud-delete-modal');
-    var version = parseInt(window.Foundation.version[0]);
 
     // Open delete modal
     $('table.crud-grid .prezent-grid-actions a.delete').on('click', function (event) {
         event.preventDefault();
         $modal.find('.button-group .delete').attr('href', $(this).attr('href'));
-
-        if (version == 5) {
-            $modal.foundation('reveal', 'open');
-        } else if (version == 6) {
-            $modal.foundation('open');
-        }
+        $modal.foundation('open');
     });
 
     // Close delete modal
     $modal.find('.button-group .cancel').on('click', function (event) {
         event.preventDefault();
-
-        if (version == 5) {
-            $modal.foundation('reveal', 'close');
-        } else if (version == 6) {
-            $modal.foundation('close');
-        }
+        $modal.foundation('close');
     });
 
 });
