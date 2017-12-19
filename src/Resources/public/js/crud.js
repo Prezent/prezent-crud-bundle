a66cf4c1d34da9ad25851696047702ea98654075
Sander Marechal <sander@prezent.nl>
2017-12-19 13:49:48 +0100

    Better reusable modals

--- src/Resources/public/js/crud.js
+++ src/Resources/public/js/crud.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
 
-    var $modal = $('#crud-delete-modal');
+    $('.crud-modal').each(function () {
+        var $modal = $(this);
+        var action = $modal.attr('data-action');
 
-    // Open delete modal
-    $('table.crud-grid .prezent-grid-actions a.delete').on('click', function (event) {
-        event.preventDefault();
-        $modal.find('.button-group .delete').attr('href', $(this).attr('href'));
-        $modal.foundation('open');
-    });
+        // Open modal
+        $('table.crud-grid .prezent-grid-actions a.' + action).on('click', function (event) {
+            event.preventDefault();
+            $modal.find('.button-group .' + action).attr('href', $(this).attr('href'));
+            $modal.foundation('open');
+        });
 
-    // Close delete modal
-    $modal.find('.button-group .cancel').on('click', function (event) {
-        event.preventDefault();
-        $modal.foundation('close');
+        // Close modal
+        $modal.find('.button-group .cancel').on('click', function (event) {
+            event.preventDefault();
+            $modal.foundation('close');
+        });
     });
 
 });
