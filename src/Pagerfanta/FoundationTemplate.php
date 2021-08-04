c086569276553cff8619fe07b5a5107f31c76b44
Sander Marechal <sander@prezent.nl>
2021-08-04 14:05:14 +0200

    Update to pagerfanta 3

--- src/Pagerfanta/FoundationTemplate.php
+++ src/Pagerfanta/FoundationTemplate.php
@@ -15,20 +15,23 @@ class FoundationTemplate extends Template
         $this->translator = $translator;
     }
 
-    protected static $defaultOptions = [
-        'prev_message' => 'pager.previous',
-        'next_message' => 'pager.next',
-        'dots_message' => '',
-        'active_suffix' => '',
-        'css_container_class' => 'pagination',
-        'css_prev_class' => 'pagination-previous',
-        'css_next_class' => 'pagination-next',
-        'css_disabled_class' => 'disabled',
-        'css_dots_class' => 'ellipsis',
-        'css_active_class' => 'current',
-    ];
+    protected function getDefaultOptions(): array
+    {
+        return [
+            'prev_message' => 'pager.previous',
+            'next_message' => 'pager.next',
+            'dots_message' => '',
+            'active_suffix' => '',
+            'css_container_class' => 'pagination',
+            'css_prev_class' => 'pagination-previous',
+            'css_next_class' => 'pagination-next',
+            'css_disabled_class' => 'disabled',
+            'css_dots_class' => 'ellipsis',
+            'css_active_class' => 'current',
+        ];
+    }
 
-    public function container()
+    public function container(): string
     {
         return sprintf(
             '<ul class="%s" role="navigation" aria-label="Pagination">%%pages%%</ul>',
@@ -36,14 +39,14 @@ class FoundationTemplate extends Template
         );
     }
 
-    public function page($page)
+    public function page($page): string
     {
         $text = $page;
 
         return $this->pageWithText($page, $text);
     }
 
-    public function pageWithText($page, $text, ?string $rel = null)
+    public function pageWithText($page, $text, ?string $rel = null): string
     {
         $class = null;
 
@@ -57,7 +60,7 @@ class FoundationTemplate extends Template
         return $this->linkLi($class, $href, $text);
     }
 
-    public function previousDisabled()
+    public function previousDisabled(): string
     {
         $class = $this->previousDisabledClass();
         $text = $this->translator->trans($this->option('prev_message'));
@@ -65,12 +68,12 @@ class FoundationTemplate extends Template
         return $this->li($class, $text);
     }
 
-    public function previousDisabledClass()
+    public function previousDisabledClass(): string
     {
         return $this->option('css_prev_class').' '.$this->option('css_disabled_class');
     }
 
-    public function previousEnabled($page)
+    public function previousEnabled($page): string
     {
         $text = $this->translator->trans($this->option('prev_message'));
         $class = $this->option('css_prev_class');
@@ -78,7 +81,7 @@ class FoundationTemplate extends Template
         return $this->pageWithTextAndClass($page, $text, $class);
     }
 
-    public function nextDisabled()
+    public function nextDisabled(): string
     {
         $class = $this->nextDisabledClass();
         $text = $this->translator->trans($this->option('next_message'));
@@ -86,12 +89,12 @@ class FoundationTemplate extends Template
         return $this->li($class, $text);
     }
 
-    public function nextDisabledClass()
+    public function nextDisabledClass(): string
     {
         return $this->option('css_next_class').' '.$this->option('css_disabled_class');
     }
 
-    public function nextEnabled($page)
+    public function nextEnabled($page): string
     {
         $text = $this->translator->trans($this->option('next_message'));
         $class = $this->option('css_next_class');
@@ -99,17 +102,17 @@ class FoundationTemplate extends Template
         return $this->pageWithTextAndClass($page, $text, $class);
     }
 
-    public function first()
+    public function first(): string
     {
         return $this->page(1);
     }
 
-    public function last($page)
+    public function last($page): string
     {
         return $this->page($page);
     }
 
-    public function current($page)
+    public function current($page): string
     {
         $text = trim($page.' '.$this->option('active_suffix'));
         $class = $this->option('css_active_class');
@@ -117,7 +120,7 @@ class FoundationTemplate extends Template
         return $this->li($class, $text);
     }
 
-    public function separator()
+    public function separator(): string
     {
         $class = $this->option('css_dots_class');
         $text = $this->option('dots_message');
@@ -125,21 +128,21 @@ class FoundationTemplate extends Template
         return $this->linkLi($class, '', $text);
     }
 
-    protected function li($class, $text)
+    protected function li($class, $text): string
     {
         $liClass = $class ? sprintf(' class="%s"', $class) : '';
 
         return sprintf('<li%s>%s</li>', $liClass, $text);
     }
 
-    protected function linkLi($class, $href, $text)
+    protected function linkLi($class, $href, $text): string
     {
         $liClass = $class ? sprintf(' class="%s"', $class) : '';
 
         return sprintf('<li%s><a href="%s">%s</a></li>', $liClass, $href, $text);
     }
 
-    protected function spanLi($class, $text)
+    protected function spanLi($class, $text): string
     {
         $liClass = $class ? sprintf(' class="%s"', $class) : '';
 
